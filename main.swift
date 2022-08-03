1dbe54537132d3c8dcead40b8e93bbd1f234c87e
Marvish <mschandr@uci.edu>
2022-08-03 14:17:51 -0700

    Completed the switch function to consider all personality identifiers.

--- main.swift
+++ main.swift
@@ -7,19 +7,69 @@ import Foundation
 class muStag:
 
 func myIntro{
-            print("Welcome to Hogwarts, as a wizard, you have the power to release a sort of spirit animal called a stag.")
+            print("Welcome to Hogwarts, as a wizard, you have the power to release a sort of spirit animal called a stag from your wand.")
         }
 func Stags{
-    stagsList = []
+    stagsList = ["Stag","Jack Russell Terrier","Otter","Horse","Hare","Phoenix","Doe","Cat","Wolf","Weasel","Lynx","Fox","Swan","Boar","Goat","Magpie"]
+    print(stagsList)
 }
 func keyIdentifier{
-    identifier == "nice"
+    print("Based on your personality, your dedicated patronus will appear in one of many forms.")
+    identifier == "heroic"
     switch {
     case 1:
-        identifier == "wolf"
+        identifier == "heroic"
+        print("Your patronus is a stag similar to Harry and his father.")
         break;
     case 2:
-        identifier == "rabbit"
+        identifier == "innocence"
+        print("Your patronus is of a doe, naturally located in a forest.")
         break;
+    case 3:
+        identifier == "intelligence"
+        print("Your patronus is of an otter, resembling Hermione's love for knowledge.")
+        break;
+    case 4:
+        identifier == "playful"
+        print("Your patronus is of an Jack Russell Terrier, symbolizing Ron's happy and go lucky nature.")
+        break;
+    case 5:
+        identifier == "youth"
+        print("Your patronus is of a horse, where Ginny expresses her strength and youthfulness seen in this animal.")
+        break;
+    case 6:
+        identifer == "witty"
+        print("Your patronus is a magpie, which the Weasley twins make because of how funny they are.")
+        break;
+    case 7:
+        identifier == "serious"
+        print("Your patronus is a weasel, resembling Ron's dad Arthur and his stern nature.")
+    case 8:
+        identifier == "bold"
+        print("Your patronus is a phoenix, which belongs with Albus Dumbledore and his powerful charisma.")
+    case 9:
+        identifier == "trickster"
+        print("Your patronus is a goat, a patronus made by Aberforth Dumbledore who had a tricky attitude.")
+    case 10:
+        identifier == "cunning"
+        print("Your patronus is a hare, which shows your tenacity and intelligence.")
+    case 11:
+        identifier == "supportive"
+        print("Your patronus resembles a wolf, a leader that has peaceful nature.")
+    case 12:
+        identifier == "orderly"
+        print("Your patronus is of a cat, which expresses your nature as stern, honest, and enforcing.")
+    case 13:
+        identifer == "subtle"
+        print("Your patronus is a fox, a fast and agile creature that makes a lot of movement.")
+    case 13:
+        identifier == "greedy"
+        print("If your patronus is of a boar, then you have a greedy and selfish attitude.")
+    case 14:
+        identifier == "attractiveness"
+        print("Your patronus as a swan demonstrates your knowledge and beauty.")
+    case 15:
+        identifer == "strength"
+        print("Your patronus as a lynx shows your ability to be smart yet open in your personality.")
     }
 }
